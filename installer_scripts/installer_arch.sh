09f1ce9ca1eb9e571e68dd1bea08819cd3ecce74
Elad Matia <elad.matia@gmail.com>
2020-03-16 11:11:04 +0200

    added installer for arch based distros and removed the libtins folder

---
+++ installer_scripts/installer_arch.sh
@@ -0,0 +1,37 @@
+# PiNAT installer for arch-based distrs, initially writted for Manjaro
+
+# Run as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Install dependencies from arch repo
+pacman -Syy cmake libpcap openssl create_ap ebtables ethtool wireless_tools --noconfirm
+
+# Install libtins
+
+git clone https://github.com/mfontanini/libtins.git
+cd libtins
+mkdir build
+cd build
+cmake ../
+make
+make install
+
+#add the library to the shared object path
+echo "/usr/local/lib" > /etc/ld.so.conf.d/libtins.conf
+
+#remove the repo
+cd ../../
+rm -rf libtins/
+
+#compile pinat
+make # build pinat library
+touch bin/__init__.py # for auto completetion
+
+#create python virtual environment and install depecdencies
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirments.txt
+deactivate
