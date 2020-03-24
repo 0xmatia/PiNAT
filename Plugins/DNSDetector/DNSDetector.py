1e233559289a09c6d0bb241bc1c07691b8a63dfd
Elad Matia <elad.matia@gmail.com>
2020-03-24 16:58:37 +0200

    switched to berserker-resolver for easier resolver managment, still can't make it work with thread pool

--- Plugins/DNSDetector/DNSDetector.py
+++ Plugins/DNSDetector/DNSDetector.py
@@ -1,4 +1,5 @@
-import dns.resolver
+from berserker_resolver import Resolver # can query multiple servers mutliple times easily
+from dns.resolver import NXDOMAIN, Timeout
 from Plugin_Observer import plugin
 from concurrent.futures import ThreadPoolExecutor
 from bin import pynat
@@ -25,9 +26,12 @@ class DNSDetector(plugin):
         self.actions = ["get_log"]
         # default num of workers is number of cores
         self.executor = ThreadPoolExecutor(max_workers=32)
+        self.resolver = Resolver()
+        self.resolver.nameservers = [
+            "1.1.1.1", "1.0.0.1", "8.8.8.8", "8.8.4.4"]
+        self.resolver.tries = 1
         self.db = ""
         self.known_ips = []
-
 
     def process(self, packet):
         dns_info = pynat.get_dns_info(packet)
@@ -38,10 +42,9 @@ class DNSDetector(plugin):
         if ips == None:  # if we cant retrieve ip layer
             return packet
         # thread = threading.Thread(target=self.dns_reslover, args=(dns_info, ips[0]))
- 
+
         # thread.daemon = True
         # thread.start()
-
         self.executor.submit(self.dns_reslover, dns_info, ips[0])
         return packet
 
@@ -49,63 +52,64 @@ class DNSDetector(plugin):
     def dns_reslover(self, dns_info, attacker_ip):
         dns_response = []
         unmateched_ips = []
-        resolver = dns.resolver.Resolver()
-        resolver.nameservers = ["8.8.8.8", "8.8.4.4"]
-        resolver.timeout = 1
-        resolver.lifetime = 1
+        # print(dns_info)
 
         for dname in dns_info:
             ip_list = dns_info[dname]
 
             try:
-                answer = resolver.query(dname, "A")
+                print("AAA")
+                answer = self.resolver.query(dname, "A")
                 print(dname)
-            except dns.resolver.NXDOMAIN:
+            except NXDOMAIN:
+                print("blue")
                 continue
-            except dns.resolver.Timeout:
+            except Timeout:
+                print("red")
                 continue
 
             for item in answer:
                 dns_response.append(item.to_text())
+
+            if dname == "sdarot.tv":
+                print(dns_response)
 
             # compare
             for suspect in ip_list:
                 if suspect not in dns_response:
                     unmateched_ips.append(suspect)
 
-            if len(unmateched_ips) == 0:  # if empty, we are good
-                continue
+        #     if len(unmateched_ips) == 0:  # if empty, we are good
+        #         continue
 
-            # change nameserver
-            resolver.nameservers = ["1.1.1.1", "1.0.0.1"]
-            try:
-                answer = resolver.query(dname, "A")
-            except dns.resolver.NXDOMAIN:
-                continue
-            except dns.resolver.Timeout:
-                continue
+        #     # change nameserver
+        #     try:
+        #         answer = self.resolver.query(dname, "A")
+        #     except dns.resolver.NXDOMAIN:
+        #         continue
+        #     except dns.resolver.Timeout:
+        #         continue
 
-            for item in answer:
-                dns_response.append(item.to_text())
+        #     for item in answer:
+        #         dns_response.append(item.to_text())
 
-            for suspect in unmateched_ips:
-                if suspect in dns_response:
-                    unmateched_ips.remove(suspect)
+        #     for suspect in unmateched_ips:
+        #         if suspect in dns_response:
+        #             unmateched_ips.remove(suspect)
 
-            for known_ip in self.known_ips:
-                if known_ip in unmateched_ips:
-                    unmateched_ips.remove(known_ip)
+        #     for known_ip in self.known_ips:
+        #         if known_ip in unmateched_ips:
+        #             unmateched_ips.remove(known_ip)
 
-            if len(unmateched_ips) != 0:
-                print("[DNSDetector] - WARNING: " + dname + \
-                    " returned different result while checking against 8.8.8.8 and \
-                        1.1.1.1. ", end="")
-                print("Suspected IP(S): " + str(unmateched_ips))
-                print()
+        #     if len(unmateched_ips) != 0:
+            print("[DNSDetector] - WARNING: " + dname +
+                    " returned different result while checking against other servers ", end="")
+            print("Suspected IP(S): " + str(unmateched_ips))
+            print()
 
-                pynat.exec_db(self.db, "INSERT OR IGNORE INTO LOG VALUES \
-                    ('{}', '{}', '{}', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'))" \
-                    .format(attacker_ip, dname, ','.join(unmateched_ips)))
+            pynat.exec_db(self.db, "INSERT OR IGNORE INTO LOG VALUES \
+                ('{}', '{}', '{}', strftime('%Y-%m-%d %H:%M', 'now', 'localtime'))"
+                            .format(attacker_ip, dname, ','.join(unmateched_ips)))
 
 
     def setup(self):
@@ -120,14 +124,11 @@ class DNSDetector(plugin):
             self.known_ips = [line.rstrip() for line in f]
         #self.known_ips = [row[0] for row in pynat.select_db(self.db, "SELECT * FROM KNOWN")]
 
-
     def teardown(self):
         pynat.close_db(self.db)
 
-
     def get_actions(self):
         return self.actions
-
 
     def get_log(self):
         answer_array = []
@@ -138,7 +139,6 @@ class DNSDetector(plugin):
                 {"attacker:": entry[0], "domain": entry[1], "suspected_ips": entry[2].split(","), "time": entry[3]})
 
         return {"result": answer_array}
-
 
     def delete_database(self):
         pynat.exec_db(self.db, "DELETE FROM LOG")
