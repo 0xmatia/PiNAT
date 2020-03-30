7c76515e010c26d296d68c76b1d29cf05c7da96b
Elad Matia <elad.matia@gmail.com>
2020-03-30 16:11:45 +0300

    The app saves the login details between runs + removed the text "Connected" while returning to the login activity

--- Client/app/src/main/java/com/pinat/pinatclient/Connect.java
+++ Client/app/src/main/java/com/pinat/pinatclient/Connect.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,26 +28,33 @@ public class Connect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData(); // load existing data if exists
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TextView data = (TextView) findViewById(R.id.data);
+        data.setText(""); // remove text from previous activity
     }
 
     public void connect(View view)
     {
+        // Get views:
+        final EditText ipAddress = (EditText)findViewById(R.id.ipAddress);
+        final EditText portNumber = (EditText)findViewById(R.id.portNumber);
+        final Button connectBtn = (Button) findViewById(R.id.connectBtn);
+        final TextView data = (TextView) findViewById(R.id.data);
+        final int port;
         final Context context = view.getContext();
+
         // Get the request queue
         RequestQueue requestQueue = VolleySingleton.getInstance(this.getApplicationContext())
                 .getRequestQueue();
 
         //Todo: Add loading animation while fetching data
 
-        // Get views:
-        EditText ipAddress = (EditText)findViewById(R.id.ipAddress);
-        EditText portNumber = (EditText)findViewById(R.id.portNumber);
-        Button connectBtn = (Button) findViewById(R.id.connectBtn);
-        final TextView data = (TextView) findViewById(R.id.data);
-
-        data.setTextColor(getResources().getColor(R.color.colorAccent));
 
         // Verify input
         Matcher matcher = Constants.IP_ADDRESS.matcher(ipAddress.getText().toString());
@@ -57,7 +65,7 @@ public class Connect extends AppCompatActivity {
         }
 
         try {
-            int port = Integer.parseInt(portNumber.getText().toString());
+            port = Integer.parseInt(portNumber.getText().toString());
             if (port < 0 || port > 65535)
             {
                 data.setText("Invalid port number!");
@@ -83,6 +91,7 @@ public class Connect extends AppCompatActivity {
                         data.setTextColor(getResources().getColor(R.color.colorSuccess));
                         data.setText("Connected! Now loading...");
 
+                        saveData(ipAddress.getText().toString(), port);
                         Intent intent = new Intent(context, General.class);
                         Bundle bundle = new Bundle();
                         bundle.putString("response", response);
@@ -104,5 +113,32 @@ public class Connect extends AppCompatActivity {
         });
 
         requestQueue.add(stringRequest);
+    }
+
+    private void saveData(String ip, int port)
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SAVED_ENDPOINT,
+                MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("ip", ip);
+        editor.putInt("port", port);
+        editor.apply();
+    }
+
+    private void loadData()
+    {
+        EditText ipAddress = (EditText)findViewById(R.id.ipAddress);
+        EditText portNumber = (EditText)findViewById(R.id.portNumber);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SAVED_ENDPOINT,
+                MODE_PRIVATE);
+        String ip = sharedPreferences.getString("ip", "");
+        int port = sharedPreferences.getInt("port", -1);
+
+        if (port != -1)
+        {
+            portNumber.setText(String.valueOf(port));
+        }
+        ipAddress.setText(ip);
     }
 }
